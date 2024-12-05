85ceaa7d247fdf9de0caa52b9d59679e1ecb2bc7
Polikiss <mymmypolly@gmail.com>
2024-12-05 15:52:51 +0300

    chore: add memory manager for array

--- schLang/src/main/java/itmo/anastasiya/MemoryManager.java
+++ schLang/src/main/java/itmo/anastasiya/MemoryManager.java
@@ -1,10 +1,10 @@
 package itmo.anastasiya;
 
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.HashMap;
 import java.util.Map;
 
 public class MemoryManager {
-    private final Map<String, ObjectEntry> memory = new ConcurrentHashMap<>();
+    private final Map<String, ObjectEntry> memory = new HashMap<>();
 
     private static class ObjectEntry {
         Object value;
@@ -16,33 +16,23 @@ public class MemoryManager {
         }
     }
 
-
-    public synchronized void allocate(String name, Object value) {
+    public void allocate(String name, Object value) {
         if (memory.containsKey(name)) {
             releaseReference(name);
         }
         memory.put(name, new ObjectEntry(value));
     }
 
-
-    public synchronized void addReference(String name) {
+    public void addReference(String name) {
         ObjectEntry entry = memory.get(name);
         if (entry != null) {
             entry.refCount++;
+        } else {
+            throw new RuntimeException("Variable " + name + " does not exist");
         }
     }
 
-    public synchronized void releaseReference(String name) {
-        deallocate(name);
-    }
-
-
-    public synchronized Object getValue(String name) {
-        ObjectEntry entry = memory.get(name);
-        return (entry != null) ? entry.value : null;
-    }
-
-    private synchronized void deallocate(String name) {
+    public void releaseReference(String name) {
         ObjectEntry entry = memory.get(name);
         if (entry != null) {
             entry.refCount--;
@@ -50,6 +40,44 @@ public class MemoryManager {
                 memory.remove(name);
                 System.out.println("Object " + name + " deallocated");
             }
+        } else {
+            throw new RuntimeException("Variable " + name + " does not exist");
         }
+    }
+
+    public Object getValue(String name) {
+        ObjectEntry entry = memory.get(name);
+        return (entry != null) ? entry.value : null;
+    }
+
+    public void allocateArray(String name, int size) {
+        if (memory.containsKey(name)) {
+            releaseReference(name);
+        }
+        memory.put(name, new ObjectEntry(new Object[size]));
+    }
+
+    public Object[] getArray(String name) {
+        ObjectEntry entry = memory.get(name);
+        if (entry != null && entry.value instanceof Object[]) {
+            return (Object[]) entry.value;
+        }
+        throw new RuntimeException("Variable " + name + " is not an array");
+    }
+
+    public void setArrayElement(String name, int index, Object value) {
+        Object[] array = getArray(name);
+        if (index < 0 || index >= array.length) {
+            throw new RuntimeException("Array index out of bounds: " + index);
+        }
+        array[index] = value;
+    }
+
+    public Object getArrayElement(String name, int index) {
+        Object[] array = getArray(name);
+        if (index < 0 || index >= array.length) {
+            throw new RuntimeException("Array index out of bounds: " + index);
+        }
+        return array[index];
     }
 }
