49c5f098986b9dcca322f77e15dbbffb1f46217d
Polikiss <mymmypolly@gmail.com>
2024-12-18 22:31:57 +0300

    chore: add jwt compiler

--- schLang/build.gradle.kts
+++ schLang/build.gradle.kts
@@ -10,6 +10,11 @@ repositories {
 }
 
 dependencies {
+    implementation("org.ow2.asm:asm:9.7")
+    implementation("org.ow2.asm:asm-tree:9.7")
+    implementation("org.ow2.asm:asm-commons:9.7")
+    implementation("org.ow2.asm:asm-util:9.7")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
